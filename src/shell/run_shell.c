4f02865bbcb32e706fc8f2a15e33fddf58d301c1
seriots <leo.giband@gmail.com>
2022-06-11 01:44:19 +0200

    dict to array

--- src/shell/run_shell.c
+++ src/shell/run_shell.c
@@ -6,15 +6,15 @@
 /*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 18:59:42 by rgarrigo          #+#    #+#             */
-/*   Updated: 2022/06/11 01:43:36 by lgiband          ###   ########.fr       */
+/*   Updated: 2022/06/11 01:44:14 by lgiband          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/types.h>
 #include <signal.h>
 #include <stdio.h>
-#include "libft.h"
-#include "minishell.h"
+#include "../../include/libft.h"
+#include "../../include/minishell.h"
 
 /*
 * Check th return value of get_commands and if return is Null, quit the program.
