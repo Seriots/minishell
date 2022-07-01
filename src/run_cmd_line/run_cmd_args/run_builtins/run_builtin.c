6c084f0895d56a376bae49ec1ce0cd421da7aad7
seriots <leo.giband@gmail.com>
2022-07-01 16:20:19 +0200

    fix

--- src/run_cmd_line/run_cmd_args/run_builtins/run_builtin.c
+++ src/run_cmd_line/run_cmd_args/run_builtins/run_builtin.c
@@ -6,7 +6,7 @@
 /*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 12:21:31 by lgiband           #+#    #+#             */
-/*   Updated: 2022/07/01 14:44:42 by lgiband          ###   ########.fr       */
+/*   Updated: 2022/07/01 16:13:10 by lgiband          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int	manage_redirections_builtin(t_shell *shell, int fd_backup[3], t_tree *cmd_line)
-{
-	/*
-	* C'ets qu'un test j'ai pas compris le fonctionnent de t_redirs, 
-	* mais avec les test que j'ai fait je me demande si on doit pas
-	* faire un join avec pwd pour l'open du fichier creer voila voila
-	* 
-	*/
-	int	fd_stdout;
-
-	(void)cmd_line;
-	fd_backup[0] = dup(0);
-	fd_backup[1] = dup(1);
-	fd_backup[2] = dup(2);
-	
-	fd_stdout = open(ft_strjoin(dict_getelem_key(shell->env, "PWD")->value, "/coucou"), O_RDWR | O_CREAT);
-	printf("fd = %d\n", fd_stdout);
-	if (fd_stdout != -1)
-		dup2 (fd_stdout, 1);
-	return (0);
-}
-
 int	backup_redirections(int fd_backup[3])
 {
 	int ret_value;
@@ -49,6 +27,25 @@ int	backup_redirections(int fd_backup[3])
 		ret_value = dup2(fd_backup[1], 1);
 	if (ret_value != -1)
 		ret_value = dup2(fd_backup[2], 2);
+	return (ret_value);
+}
+
+int	manage_redirections_builtin(t_shell *shell, int fd_backup[3], t_tree *cmd_line)
+{
+	/*
+	* C'ets qu'un test j'ai pas compris le fonctionnent de t_redirs, 
+	* mais avec les test que j'ai fait je me demande si on doit pas
+	* faire un join avec pwd pour l'open du fichier creer voila voila
+	* 
+	*/
+	int	ret_value;
+
+	fd_backup[0] = dup(0);
+	fd_backup[1] = dup(1);
+	fd_backup[2] = dup(2);
+	ret_value = manage_redirections(cmd_line);
+	if (ret_value)
+		backup_redirections(fd_backup);
 	return (ret_value);
 }
 
@@ -109,20 +106,4 @@ int	run_builtin(t_shell *shell, t_tree *cmd_line)
 	ret_value = execute_builtins(shell, cmd_line);
 	backup_redirections(fd_backup);
 	return (ret_value);
-}
-
-int	main(int argc, char *argv[], char **env)
-{
-	t_shell shell;
-	char	**copy;
-	int		ret_value;
-
-	init_shell(&shell, env);
-	copy = ft_calloc(sizeof(char *), argc);
-	if (!copy)
-		return (0);
-	for (int i = 0; i < argc - 1; i++)
-		copy[i] = ft_strdup(argv[i + 1]);
-	//ret_value = run_builtin(&shell, copy);
-	free_shell(&shell);
 }
