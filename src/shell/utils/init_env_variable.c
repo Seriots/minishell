3cfe245dbdccec07853c8a82e97fa8f341c15381
seriots <leo.giband@gmail.com>
2022-07-04 23:50:06 +0200

    clean and norm

--- src/shell/utils/init_env_variable.c
+++ src/shell/utils/init_env_variable.c
@@ -6,7 +6,7 @@
 /*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 22:50:05 by lgiband           #+#    #+#             */
-/*   Updated: 2022/06/16 11:40:19 by lgiband          ###   ########.fr       */
+/*   Updated: 2022/07/04 23:37:27 by lgiband          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,114 +16,12 @@
 #include "../../../include/minishell.h"
 #include <stdlib.h>
 
-static int	free_and_set_error(char *key, char *value, int *error)
+int	free_and_set_error(char *key, char *value, int *error)
 {
 	free(key);
 	free(value);
 	*error -= 1;
 	return (0);
-}
-
-static int	add_pwd(t_dict **env, int *error)
-{
-	t_dict	*new;
-	char	*key;
-	char	*value;
-
-	value = 0;
-	key = 0;
-	key = ft_calloc(sizeof(char), 4);
-	if (!key)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(key, "PWD", 4);
-	value = get_current_directory();
-	if (!value)
-		return (free_and_set_error(key, value, error));
-	new = dict_new(key, value);
-	if (!new)
-		return (free_and_set_error(key, value, error));
-	dict_add_back(env, new, free, free);
-	return (*error);
-}
-
-static int	add_shlvl(t_dict **env, int *error)
-{
-	t_dict	*new;
-	char	*key;
-	char	*value;
-
-	value = 0;
-	key = 0;
-	key = ft_calloc(sizeof(char), 6);
-	if (!key)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(key, "SHLVL", 6);
-	value = ft_calloc(sizeof(char), 2);
-	if (!value)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(value, "1", 2);
-	new = dict_new(key, value);
-	if (!new)
-		return (free_and_set_error(key, value, error));
-	dict_add_back(env, new, free, free);
-	return (*error);
-}
-
-static int	update_shlvl(t_dict **search, int *error)
-{
-	char	*new_shlvl;
-
-	new_shlvl = ft_itoa(ft_atoi((*search)->value) + 1);
-	if (!new_shlvl)
-	{
-		*error -= 1;
-		return (-1);
-	}
-	free ((*search)->value);
-	(*search)->value = new_shlvl;
-	return (*error);
-}
-
-static int	add_envpath(t_dict **env, int *error)
-{
-	t_dict	*new;
-	char	*key;
-	char	*value;
-
-	value = 0;
-	key = 0;
-	key = ft_calloc(sizeof(char), 2);
-	if (!key)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(key, "_", 2);
-	value = ft_calloc(sizeof(char), 13);
-	if (!value)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(value, "/usr/bin/env", 13);
-	new = dict_new(key, value);
-	if (!new)
-		return (free_and_set_error(key, value, error));
-	dict_add_back(env, new, free, free);
-	return (*error);
-}
-
-static int	add_oldpwd(t_dict **env, int *error)
-{
-	t_dict	*new;
-	char	*key;
-	char	*value;
-
-	value = 0;
-	key = 0;
-	key = ft_calloc(sizeof(char), 7);
-	if (!key)
-		return (free_and_set_error(key, value, error));
-	ft_strlcpy(key, "OLDPWD", 7);
-	new = dict_new(key, value);
-	if (!new)
-		return (free_and_set_error(key, value, error));
-	dict_add_back(env, new, free, free);
-	return (*error);
 }
 
 int	set_default_variable(t_shell **shell)
---
+++ src/shell/utils/init_pwd.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_pwd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 23:29:04 by lgiband           #+#    #+#             */
+/*   Updated: 2022/07/04 23:37:58 by lgiband          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/dict.h"
+#include "../../../include/libft.h"
+#include "../../../include/ft_printf.h"
+#include "../../../include/minishell.h"
+#include <stdlib.h>
+
+int	add_pwd(t_dict **env, int *error)
+{
+	t_dict	*new;
+	char	*key;
+	char	*value;
+
+	value = 0;
+	key = 0;
+	key = ft_calloc(sizeof(char), 4);
+	if (!key)
+		return (free_and_set_error(key, value, error));
+	ft_strlcpy(key, "PWD", 4);
+	value = get_current_directory();
+	if (!value)
+		return (free_and_set_error(key, value, error));
+	new = dict_new(key, value);
+	if (!new)
+		return (free_and_set_error(key, value, error));
+	dict_add_back(env, new, free, free);
+	return (*error);
+}
+
+int	add_oldpwd(t_dict **env, int *error)
+{
+	t_dict	*new;
+	char	*key;
+	char	*value;
+
+	value = 0;
+	key = 0;
+	key = ft_calloc(sizeof(char), 7);
+	if (!key)
+		return (free_and_set_error(key, value, error));
+	ft_strlcpy(key, "OLDPWD", 7);
+	new = dict_new(key, value);
+	if (!new)
+		return (free_and_set_error(key, value, error));
+	dict_add_back(env, new, free, free);
+	return (*error);
+}
