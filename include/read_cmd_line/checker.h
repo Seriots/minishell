dbfdbcb35cfdc9da81d247df6bfca7147b8bc16e
Romain GARRIGOU <rgarrigo@e1r3p1.clusters.42paris.fr>
2022-07-12 19:55:01 +0200

    Debug checker after parenthesis

--- include/read_cmd_line/checker.h
+++ include/read_cmd_line/checker.h
@@ -6,7 +6,7 @@
 /*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 02:16:00 by rgarrigo          #+#    #+#             */
-/*   Updated: 2022/07/09 20:33:48 by rgarrigo         ###   ########.fr       */
+/*   Updated: 2022/07/12 19:53:53 by rgarrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 # define ALLOWED_NEWLINE 0
 # define ALLOWED_PAR_LEFT 0
 # define ALLOWED_PAR_RIGHT 57 // (1 + 8 + 16 + 32)
-# define ALLOWED_OR 2050 // (2 + 2048)
-# define ALLOWED_AND 2050 // (2 + 2048)
-# define ALLOWED_PIPE 2050 // (2 + 2048)
+# define ALLOWED_OR 4034 // (2 + 64 + 128 + 256 + 512 + 1024 + 2048)
+# define ALLOWED_AND 4034 // (2 + 64 + 128 + 256 + 512 + 1024 + 2048)
+# define ALLOWED_PIPE 4034 // (2 + 64 + 128 + 256 + 512 + 1024 + 2048)
 # define ALLOWED_RED_HEREDOC 61 // (1 + 4 + 8 + 16 + 32)
 # define ALLOWED_RED_STDIN 61 // (1 + 4 + 8 + 16 + 32)
 # define ALLOWED_RED_APPEND_STDOUT 61 // (1 + 4 + 8 + 16 + 32)
