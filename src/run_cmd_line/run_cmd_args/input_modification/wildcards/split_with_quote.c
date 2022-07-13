6d0ea5be762e273c2256dbde88650fb8036d0d04
Leo Giband <leo.giband@gmail.com>
2022-07-13 22:40:46 +0200

    fix split with quote

--- src/run_cmd_line/run_cmd_args/input_modification/wildcards/split_with_quote.c
+++ src/run_cmd_line/run_cmd_args/input_modification/wildcards/split_with_quote.c
@@ -6,7 +6,7 @@
 /*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 20:47:44 by lgiband           #+#    #+#             */
-/*   Updated: 2022/07/13 22:15:56 by lgiband          ###   ########.fr       */
+/*   Updated: 2022/07/13 22:38:41 by lgiband          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,30 @@
 #include "wildcards.h"
 #include <stdlib.h>
 
+#include <stdio.h>
+
 int	add_to_lst(t_list **lst_addr, char **array)
 {
+	static	int	oui;
 	t_list	*new;
 	int		i;
 
 	i = 0;
 	while (array[i])
 	{
-		new = list_new(array[i]);
+		if (oui)
+			new = list_new(array[i]);
+		else
+			new = 0;
 		if (!new)
+		{
+			while (array[i])
+			{
+				free(array[i]);
+				i ++;
+			}
 			return (1);
+		}
 		list_add_back(lst_addr, new);
 		i ++;
 	}
@@ -43,12 +56,12 @@ char	**split_with_quote(char	**input)
 	list_input = 0;
 	while (input[i])
 	{
-		split_result = ft_split_quoted(input[i], " ");
+		split_result = ft_split_quoted(input[i], " \t\n\r\v\f");
 		if (!split_result)
 			return (ft_free_tab(input), list_clear(&list_input, free), (void *)0);
 		error = add_to_lst(&list_input, split_result);
 		if (error)
-			return (ft_free_tab(input), ft_free_tab(split_result), list_clear(&list_input, NULL), (void *)0);
+			return (ft_free_tab(input), list_clear(&list_input, free), free(split_result), (void *)0);
 		free(split_result);
 		i ++;
 	}
